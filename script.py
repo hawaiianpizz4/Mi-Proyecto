1ef3b1712d996a7c7d4945991e0cbc97a44a650a
Jair Morocho <jairmorocho99@gmail.com>
2020-12-21 19:58:32 -0500

    Modificando script.py

--- script.py
+++ script.py
@@ -2,10 +2,12 @@
 # Autor: Josué Morales <rtufino@ups.edu.ec>
 # Fecha: 23-06-2020
 # Versión: 1.0.0
+# Modificado por Jair Morocho
 
 def main():
     nombre= input("Cual es tu nombre? ") 
     print("Hola", nombre, "Bienvenido a Git!")
+    print("Hola soy Jair Alexis")
 
 
 if __name__ == "__main__":
